3e99bee5d185ef3056e3dcdf0b9b5ae9a0e4d738
Taranveer Virk <taranveer@virk.cc>
2018-12-10 12:20:08 -0500

    fix(lodash.padstart): export as a module
    This export allows for the following type of import:
    `import {padStart} from 'loadsh.padstart';` vs. the current export requires `const padStart = require('lodash.padstart')`

--- types/lodash.padstart/index.d.ts
+++ types/lodash.padstart/index.d.ts
@@ -7,4 +7,4 @@
 // Generated from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/lodash/scripts/generate-modules.ts
 
 import { padStart } from "lodash";
-export = padStart;
+export padStart;
